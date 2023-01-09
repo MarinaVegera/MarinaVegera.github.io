f717c123fd9330ec118419f2d854d0f1604db7fa
Marina Vegera <maribn182@gmail.com>
2023-01-10 00:05:59 +0300

    project_stage5

--- IT-Academy-Final-Project/js/main_page.js
+++ IT-Academy-Final-Project/js/main_page.js
@@ -1,50 +1,12 @@
-/*function toggleCategoryInput() {
-    const box = document.querySelector('.category-radio-block');
-    if (box.classList.contains('hide')) {
-        box.classList.remove('hide')
-    } else {
-        box.classList.add('hide')
-    }
-}*/
 function openContent(type) {
-    const dialog = document.querySelector('.choice-radio-container-'+type);
+    const dialog = document.querySelector('.choice-radio-container-' + type);
     dialog.classList.toggle('shown');
-    const item = document.querySelector('.expand-item-'+type);
+    const item = document.querySelector('.expand-item-' + type);
     item.classList.toggle('add-expand-rotate');
 }
-/*function openContentCategory() {
-    const dialog = document.querySelector('.choice-radio-container-category');
-    dialog.classList.toggle('shown');
-    const item = document.querySelector('.expand-item-category');
-    item.classList.toggle('add-expand-rotate');
-}
-function openContentWhom() {
-    const dialog = document.querySelector('.choice-radio-container-whom');
-    dialog.classList.toggle('shown');
-    const item = document.querySelector('.expand-item-whom');
-    item.classList.toggle('add-expand-rotate');
-}
-function openContentPrice() {
-    const dialog = document.querySelector('.choice-radio-container-price');
-    dialog.classList.toggle('shown');
-    const item = document.querySelector('.expand-item-price');
-    item.classList.toggle('add-expand-rotate');
-}*/
-/*function changeCategory() {
-    const box = document.querySelector('#box');
-    const categoryInput = document.querySelector('#category-input');
 
-    const cate = colorInput.value;
-
-    box.style.backgroundColor = color;
-
-    console.log(box.style.backgroundColor);
-    console.log(getComputedStyle(box).color);
-}*/
-
-/*document.querySelector('.radio-item').addEventListener('click', () => */
 function changeCategory(type) {
-    let radioCategory = document.querySelectorAll('.radio-item-'+type);
+    let radioCategory = document.querySelectorAll('.radio-item-' + type);
     let data;
     for (let i = 0; i < radioCategory.length; i++) {
         if (radioCategory[i].checked) {
@@ -52,8 +14,7 @@ function changeCategory(type) {
             break;
         }
     }
-   /* document.querySelector('#category-input').value = data;*/
-    document.querySelector('.value-choice-'+type).textContent = data;
+    document.querySelector('.value-choice-' + type).textContent = data;
 }
 
 let currentImageIndex = 0;
@@ -65,11 +26,10 @@ function goToNext(step) {
     const imageContainer = document.querySelector('.inner-offering-container');
     imageContainer.style.transform = `translateX(-${offset}px)`
     disableButtons();
-    
+
 }
 function disableButtons() {
     const [prev, next] = document.querySelectorAll('.carousel-offering-button');
-    /*console.log(currentImageIndex);*/
     if (currentImageIndex === 0) {
         prev.setAttribute('disabled', 'true')
     } else {
